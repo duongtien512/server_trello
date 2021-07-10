4cecdaff60b70726c744e447bcde547d90641c0c
duongtien512 <58157764+duongtien512@users.noreply.github.com>
2021-07-10 14:40:42 +0700

    create project

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { mapOrder } from './utilities/sorts.js';
+
+const app = express();
+
+const hostname = 'localhost';
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.end('<h1>Hello</h1>')
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at ${hostname}:${port}`);
+})
