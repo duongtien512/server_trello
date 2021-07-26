fa94e66e7ad767fbf5972a34b4842af4810a8db6
duongtieu741 <70996624+duongtieu741@users.noreply.github.com>
2021-07-26 09:32:40 +0700

    update design database

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,25 +3,23 @@ import {env} from '*/config/env';
 
 // Oob7DsjJ06VJNdxA
 
+let dbInstance = null;
+
 export const connectDB = async () => {
     const client = new MongoClient(env.MONGODB_URI, {
         useUnifiedTopology: true,
         useNewUrlParser: true
     });
-    try {
-        // connect the client to server
-        await client.connect();
-        console.log('Connect successfully to server!');
 
-        // list databases
-        await listDatabases(client);
-    } finally {
-        // ngat ket noi toi db
-        await client.close();
-    }
+    // connect the client to server
+    await client.connect();
+
+    dbInstance = client.db(env.DATABASE_NAME )
+
 };
 
-const listDatabases = async (client) => {
-    const databases = await client.db().admin().listDatabases();
-    databases.databases.forEach(db => console.log(`${db.name}`))
-};
+// get database instance
+ export const getDB = () => {
+    if(!dbInstance) throw new Error('Must connection Database first!');
+    return dbInstance;
+ }
